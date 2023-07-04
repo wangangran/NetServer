8cc149c0edddeb8566c6edf8433d24c36be05096
war <58344556@qq.com>
2023-07-04 16:52:11 +0800

    project sync

--- study/c++/Test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main(int argc, char **argv) {
-    std::string cmd = "tar --use-compress-program=\"pigz -p2\" -xvpf ";
-	std::cout << cmd << std::endl;
-    return 0;
-}
